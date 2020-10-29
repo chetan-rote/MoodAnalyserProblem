74fbd0d02c9b67bfa127cdc9e380a8814ffbb607
Chetan <Chetan@DESKTOP-9SU09BS>
2020-10-29 20:50:08 +0530

    [Chetan]Add. TC1.1 and TC1.2.

---
+++ MoodAnalyserTest/MoodAnalyzerTest.cs
@@ -0,0 +1,39 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MoodAnalyserProblem;
+namespace MoodAnalyserTest
+{
+    [TestClass]
+    public class MoodAnalyzerTest
+    {
+        /// <summary>
+        /// Givens the I am in sad mood should return sad.
+        /// </summary>
+        [TestMethod]
+        public void GivenIAmInSadMood_ShouldReturnSad()
+        {
+            //Arrange
+            string message = "I am in sad mood.";
+            string expectedValue = "SAD";
+            MoodAnalyser moodAnalyser = new MoodAnalyser(message);
+            //Act
+            string result = moodAnalyser.AnalyseMood(message);
+            //Assert
+            Assert.AreEqual(expectedValue, result);
+        }
+        /// <summary>
+        /// Givens the I am in any mood should return happy.
+        /// </summary>
+        [TestMethod]
+        public void GivenIAmInAnyMood_ShouldReturnHappy()
+        {
+            //Arrange
+            string message = "I am in happy mood.";
+            string expectedValue = "HAPPY";
+            MoodAnalyser moodAnalyzer = new MoodAnalyser(message);
+            //Act
+            string result = moodAnalyzer.AnalyseMood(message);
+            //Assert
+            Assert.AreEqual(expectedValue, result);
+        }
+    }
+}
