1a91756b02fa931a7788d391e7e3d5ec4193e924
Chetan <Chetan@DESKTOP-9SU09BS>
2020-10-30 22:25:07 +0530

    [Chetan]Add. Reflection to create constructor.

---
+++ MoodAnalyserProblem/MoodAnalyserFactory.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace MoodAnalyserProblem
+{
+    public class MoodAnalyserFactory
+    {
+        public static object CreateMoodAnalyserObject(string className, string constructorName)
+        {
+            string pattern = @"." + constructorName + "$";
+            Match result = Regex.Match(className, pattern);
+
+            if (result.Success)
+            {
+                try
+                {
+                    Assembly executing = Assembly.GetExecutingAssembly();
+                    Type moodAnalyseType = executing.GetType(className);
+                    return Activator.CreateInstance(moodAnalyseType);
+                }
+                catch (ArgumentNullException)
+                {
+                    throw new MoodAnalysisCustomException(MoodAnalysisCustomException.ExceptionType.NO_SUCH_CLASS, "class not found.");
+                }
+            }
+            else
+            {
+                throw new MoodAnalysisCustomException(MoodAnalysisCustomException.ExceptionType.NO_SUCH_METHOD, "constructor not found.");
+            }
+        }
+
+    }
+}
